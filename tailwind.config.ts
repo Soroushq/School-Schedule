21a16bf04bb517e3a83046c0d8a3cb027a13de2b
Soroush <166734554+Soroushq@users.noreply.github.com>
2025-05-04 13:26:12 +0330

    add farhang fonts

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Farhang2', 'Vazirmatn', 'sans-serif'],
+        mono: ['Farhang2', 'Vazirmatn', 'monospace'],
+        farhang: ['Farhang2', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        demi: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config; 
